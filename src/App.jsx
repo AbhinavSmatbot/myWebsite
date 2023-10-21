7af6810646263641933e8757833babbd2e253d89
AbhinavSmatbot <Abhinav.smatbot@gmail.com>
2023-10-22 01:42:56 +0530

    projrct

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from './components/Home/Home';
+import Header from './components/Home/Header/Header';
+import Footer from './components/Home/Footer/Footer';
+
+const App = () => {
+  return (
+  <>
+    <Header/>
+    <Home/>
+    <Footer/>
+  </>
+  )
+}
+
+export default App
